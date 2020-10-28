b54e489cc5766ac49be03875ddc69fd14dac4fc1
the-vampiire <vampiirecodes@gmail.com>
2020-10-28 16:07:08 -0500

    test: IntegrationTestConfig wrapper annotation

---
+++ src/test/java/org/launchcode/devops/mapnotesapi/IntegrationTestConfig.java
@@ -0,0 +1,18 @@
+package org.launchcode.devops.mapnotesapi;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface IntegrationTestConfig {
+}

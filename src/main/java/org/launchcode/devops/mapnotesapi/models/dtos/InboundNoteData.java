7ef383df2fd7065bd07c756218231d051ff36c09
the-vampiire <vampiirecodes@gmail.com>
2020-10-30 13:18:02 -0500

    models: InboundNoteData DTO with validations

---
+++ src/main/java/org/launchcode/devops/mapnotesapi/models/dtos/InboundNoteData.java
@@ -0,0 +1,14 @@
+package org.launchcode.devops.mapnotesapi.models.dtos;
+
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+
+@Data
+public class InboundNoteData {
+  @Size(min = 1, message = "The body must not be empty")
+  private String body;
+
+  @Size(min = 1, max = 32, message = "Title must be between 1 and 32 characters")
+  private String title;
+}

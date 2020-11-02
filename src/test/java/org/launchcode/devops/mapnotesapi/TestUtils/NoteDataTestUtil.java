be7065f68a36bb9d256043242997ac98e4c1d4d7
the-vampiire <vampiirecodes@gmail.com>
2020-11-01 19:37:02 -0600

    tests: NoteDataTestUtil change to Service component

--- src/test/java/org/launchcode/devops/mapnotesapi/TestUtils/NoteDataTestUtil.java
+++ src/test/java/org/launchcode/devops/mapnotesapi/TestUtils/NoteDataTestUtil.java
@@ -3,9 +3,9 @@ package org.launchcode.devops.mapnotesapi.TestUtils;
 import org.launchcode.devops.mapnotesapi.data.NoteData;
 import org.launchcode.devops.mapnotesapi.models.Note.NoteEntity;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.TestComponent;
+import org.springframework.stereotype.Service;
 
-@TestComponent
+@Service
 public class NoteDataTestUtil {
   @Autowired
   private NoteData noteData;

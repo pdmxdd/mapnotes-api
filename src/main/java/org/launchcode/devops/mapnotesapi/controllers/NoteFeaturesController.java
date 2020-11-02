4d2f2fb6b877743dfaa512dfe8afacef7d3a9c51
pdmxdd <pdmxdd@gmail.com>
2020-11-02 16:41:15 -0600

    added crossorigin globs
    manual testing from client

--- src/main/java/org/launchcode/devops/mapnotesapi/controllers/NoteFeaturesController.java
+++ src/main/java/org/launchcode/devops/mapnotesapi/controllers/NoteFeaturesController.java
@@ -14,6 +14,7 @@ import org.launchcode.devops.mapnotesapi.models.Note.NoteEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/notes/{noteId}/features", produces = MediaType.APPLICATION_JSON_VALUE)
+@CrossOrigin(value = "*")
 public class NoteFeaturesController {
 
     @Autowired
--- src/main/java/org/launchcode/devops/mapnotesapi/controllers/NotesController.java
+++ src/main/java/org/launchcode/devops/mapnotesapi/controllers/NotesController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/notes", produces = MediaType.APPLICATION_JSON_VALUE)
+@CrossOrigin(value = "*")
 public class NotesController {
   @Autowired
   private NoteData noteData;
